90c21dc4f43f479706ea1c52ae5cccb0acbe3f53
Phil Salant <PSalant@gmail.com>
2020-06-13 20:25:06 -0400

    Initial commit, preview site

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
+
+func main() {
+	http.HandleFunc("/", previewHandler)
+	// http.HandleFunc("/", aboutHandler)
+	// http.HandleFunc("/accommodations", accommodationsHandler)
+	// http.HandleFunc("/rsvp", rsvpHandler)
+
+	fs := http.FileServer(http.Dir("assets/"))
+	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func previewHandler(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("./templates/preview.html")
+
+	err := t.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+// func aboutHandler(w http.ResponseWriter, r *http.Request) {
+// 	t, _ := template.ParseFiles("./templates/about.html")
+// 	t.Execute(w, nil)
+// }
+
+// func accommodationsHandler(w http.ResponseWriter, r *http.Request) {
+// 	t, _ := template.ParseFiles("./templates/accommodations.html")
+// 	t.Execute(w, nil)
+// }
+
+// func rsvpHandler(w http.ResponseWriter, r *http.Request) {
+// 	t, _ := template.ParseFiles("./templates/rsvp.html")
+// 	t.Execute(w, nil)
+// }
