525ded258d06fb9096aab8033bf4b7b946c71a25
Phil Salant <PSalant@gmail.com>
2020-06-23 20:09:31 -0400

    Add configuration for hosting with AWS Elastic Beanstalk
    Includes necessary config files for building and running the app,
    as well as static file serving. Also moves the `/templates` directory
    to `/assets/html`.

--- src/main.go
+++ src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"text/template"
 )
@@ -14,8 +15,14 @@ const (
 
 var templates = template.Must(template.ParseFiles(getTemplateFiles()...))
 
+type Page struct {
+	Title string
+	Email []byte
+}
+
 func main() {
 	http.HandleFunc("/", previewHandler)
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) { w.WriteHeader(http.StatusOK) })
 	// http.HandleFunc(ABOUT_PATH, makeHandler(ABOUT_PATH))
 	// http.HandleFunc(ACCOMMODATIONS_PATH, makeHandler(ACCOMMODATIONS_PATH))
 	// http.HandleFunc(RSVP_PATH, makeHandler(RSVP_PATH))
@@ -23,18 +30,22 @@ func main() {
 	fs := http.FileServer(http.Dir("assets/"))
 	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
 
-	startServer()
+	log.Fatal(http.ListenAndServe(":5000", nil))
 }
 
 func previewHandler(w http.ResponseWriter, r *http.Request) {
-	if err := renderTemplate(w, PREVIEW_PATH); err != nil {
+	p := &Page{Title: "Preview"}
+
+	if err := renderTemplate(w, PREVIEW_PATH, p); err != nil {
 		http.Error(w, err.Error(), http.StatusNotFound)
 	}
 }
 
 // func makeHandler(path string) http.HandlerFunc {
+// 	p := &Page{Title: strings.Title(path[1:])}
+
 // 	return func(w http.ResponseWriter, r *http.Request) {
-// 		if err := renderTemplate(w, path); err != nil {
+// 		if err := renderTemplate(w, path, p); err != nil {
 // 			http.Redirect(w, r, "/", http.StatusFound)
 // 		}
 // 	}
